3a1e45fadd2f204231bcb254d0b84bc4a6376b46
Resacky <Ruiz-joseph@hotmail.com>
2022-05-05 09:39:22 -0400

    Modified my code to be up to date

--- Rotor.py
+++ Rotor.py
@@ -1,12 +1,58 @@
+
+
+
+# This method is to create the individual rotors with already pre-generated configurations
+def CreateRotor(name, rotorPOS, ringSetting):
+    name = name.upper()
+
+    if name == "I":
+        return __init__(name, "EKMFLGDQVZNTOWYHXUSPAIBRCJ", rotorPOS, 16, ringSetting)
+    elif name == "II":
+        return __init__(name, "AJDKSIRUXBLHWTMCQGZNPYFVOE", rotorPOS, 4, ringSetting)
+    elif name == "III":
+        return __init__(name, "BDFHJLCPRTXVZNYEIWGAKMUSQO", rotorPOS, 21, ringSetting)
+    elif name == "IV":
+        return __init__(name, "ESOVPZJAYQUIRHXLNFTGKDCMWB", rotorPOS, 9, ringSetting)
+    elif name == "V":
+        return __init__(name, "VZBRGITYUPSDNHLXAWMJQOFECK", rotorPOS, 25, ringSetting)
+    elif name == "VI":
+        return __init__(name, "JPGVOUMFYQBENHZRDKASXLICTW", rotorPOS, 0, ringSetting)
+
+        @override
+        def isAtNotch(self):
+            return self.rotorPOS == 12 or self.rotorPOS == 25
+
+    elif name == "VII":
+        return __init__(name, "NZJHGRCXMYSWBOUFAIVLPEKQDT", rotorPOS, 0, ringSetting)
+
+        @override
+        def isAtNotch(self):
+            return self.rotorPOS == 12 or self.rotorPOS == 25
+
+    elif name == "VIII":
+        return __init__(name, "FKQHTLXOCBJSPDZRAMEWNIUYGV", rotorPOS, 0, ringSetting)
+
+        @override
+        def isAtNotch(self):
+            return self.rotorPOS == 12 or self.rotorPOS == 25
+
+    else:
+        return __init__("Identity", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", rotorPOS, 0, ringSetting)
+
+
 def decodeWiring(encoding):
     return [x - 65 for x in list(encoding)]
 
 
-def __init__(self, name, encoding, rotorPOS, notchPOS, ringSetting):
+def inverseWiring(primaryWiring):
+    return [primaryWiring[(x + 1) * -1] for x in range(len(primaryWiring))]
 
+
+def __init__(self, name, encoding, rotorPOS, notchPOS, ringSetting):
     self.name = name
-    #self.forwardWiring[] =
-    #self.backwardWiring[] =
+    self.forwardWiring = decodeWiring(encoding)
+    self.backwardWiring = inverseWiring(self.forwardWiring)
     self.rotorPOS = rotorPOS
     self.notchPOS = notchPOS
     self.ringSetting = ringSetting
+
